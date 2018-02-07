2d36c9558ae8a5600477aed9bba32a0c7b1152f6
Hong Zhang <wora2000@gmail.com>
2018-02-07 10:02:41 -0800

    Use a util function to create the client.

--- go/src/example/google/main.go
+++ go/src/example/google/main.go
@@ -9,19 +9,27 @@ import proto "github.com/golang/protobuf/proto"
 import status "google.golang.org/genproto/googleapis/rpc/status"
 import google "golang.org/x/oauth2/google"
 
+func NewClient(ctx context.Context, baseUrl string) (*client.Client, error) {
+	http, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/cloud-platform")
+	if err != nil {
+		return nil, err
+	}
+	c := &client.Client{
+		BaseURL:     baseUrl,
+		HTTP:        http,
+		ContentType: "application/x-protobuf",
+		UserAgent:   "protorpc/0.1",
+		Status:      &status.Status{},
+	}
+	return c, nil
+}
+
 func main() {
 	ctx := context.Background()
-	http, err := google.DefaultClient(ctx, "https://www.googleapis.com/auth/cloud-platform")
+	c, err := NewClient(ctx, "...")
 	if err != nil {
 		fmt.Print(err.Error())
 		return
-	}
-	c := &client.Client{
-		BaseURL: "...",
-		HTTP: http,
-		ContentType: "application/x-protobuf",
-		UserAgent: "listservices/0.1",
-		Status: &status.Status{},
 	}
 	request := &sm.ListServicesRequest{}
 	response := &sm.ListServicesResponse{}
